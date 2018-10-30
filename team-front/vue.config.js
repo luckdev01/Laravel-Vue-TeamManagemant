cfccab104f920b01ed80aadf744ea26cab394b06
sinstar2000 <strongweb102@gmail.com>
2018-10-30 00:26:52 +0000

    added svg loader

--- team-front/vue.config.js
+++ team-front/vue.config.js
@@ -1,3 +1,8 @@
+var path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, './', dir)
+}
 module.exports = {
     outputDir: '../public',
     indexPath: process.env.NODE_ENV === 'production'
@@ -10,5 +15,22 @@ module.exports = {
             data: `@import "@/styles/index.scss";`
           }
         }
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/icons'))
+            .end()
+
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
     }
-  }
+}
